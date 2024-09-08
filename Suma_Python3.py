b30a16b73ab8fa51e3b8c835ada3e149461bb21c
louisx172 <93785145+louisx172@users.noreply.github.com>
2024-09-08 16:21:05 -0600

    Add files via upload

---
+++ Suma_Python3.py
@@ -0,0 +1,12 @@
+import sys
+
+# Definimos dos números
+numero1 = 5
+numero2 = 7
+
+# Realizamos la suma
+suma = numero1 + numero2
+
+# Mostramos el resultado
+print(f"La suma de {numero1} y {numero2} es: {suma}")
+sys.stdout.flush()

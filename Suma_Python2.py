7ac53e2188efb87c54a9fef431f170097aac47bd
louisx172 <93785145+louisx172@users.noreply.github.com>
2024-09-08 11:45:54 -0600

    Add files via upload

---
+++ Suma_Python2.py
@@ -0,0 +1,9 @@
+# Definimos dos números
+numero1 = 5
+numero2 = 7
+
+# Realizamos la suma
+suma = numero1 + numero2
+
+# Mostramos el resultado
+print(f"La suma de {numero1} y {numero2} es: {suma}")

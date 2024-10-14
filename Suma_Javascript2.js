13c09dcd2e47cb967d2e3db483715d12fc6ea044
louisx172 <93785145+louisx172@users.noreply.github.com>
2024-10-13 23:01:31 -0600

    Delete Suma_Javascript2.js

--- Suma_Javascript2.js
+++
@@ -1,6 +0,0 @@
-const a = parseFloat(prompt("Ingrese el valor de a:"));
-const b = parseFloat(prompt("Ingrese el valor de b:"));
-
-const resultado = a + b;
-
-console.log(`El resultado de la suma es: ${resultado}`);

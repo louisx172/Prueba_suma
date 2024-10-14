dc7f872cd32cd4710ec64eaea73c729aacadd9bf
louisx172 <93785145+louisx172@users.noreply.github.com>
2024-10-13 23:02:21 -0600

    Delete Suma_Python2.py

--- Suma_Python2.py
+++
@@ -1,9 +0,0 @@
-# Definimos dos números
-numero1 = 5
-numero2 = 10
-
-# Realizamos la suma
-suma = numero1 + numero2
-
-# Mostramos el resultado
-print(f"La suma de {numero1} y {numero2} es: {suma}")

aff80d6cc527877b8c0cc09cc38435629e86b15a
louisx172 <93785145+louisx172@users.noreply.github.com>
2025-02-01 19:18:39 -0600

    Create Multiplicacion3Nums.py

---
+++ Multiplicacion3Nums.py
@@ -0,0 +1,8 @@
+import sys
+
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+c = float(sys.argv[3])
+
+resultado = a * b * c
+print(f"El resultado de la multiplicación de los tres números es: {resultado}")
